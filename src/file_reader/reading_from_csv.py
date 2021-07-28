a76dac37929f2f7a8d68b0c84dfcec3860397453
Patryk Ostrowski <patrykost1998@gmail.com>
2021-07-28 13:11:04 +0200

    Some style reformat

--- src/file_reader/reading_from_csv.py
+++ src/file_reader/reading_from_csv.py
@@ -6,15 +6,6 @@ from file_reader.csvreader import csv_reader
 from file_reader.db import import_to_db
 
 
-
-def _define_headers_to_read(file):
-    filename = file.split("/")[-1]
-    if re.match("^routes-", filename):
-        return ["route_id", "route_short_name", "route_desc"]
-    elif re.match("^cities", filename):
-        return ["city_id", "city_name"]
-
-
 def get_files(
     dir_path: str = os.path.abspath(os.path.join(os.getcwd(), os.pardir)),
     dir_name: str = "data",
@@ -26,6 +17,14 @@ def get_files(
         if os.path.isfile(os.path.join(full_path, file))
     ]
     return [os.path.join(full_path, file) for file in files]
+
+
+def _define_headers_to_read(file):
+    filename = file.split("/")[-1]
+    if re.match("^routes-", filename):
+        return ["route_id", "route_short_name", "route_desc"]
+    elif re.match("^cities", filename):
+        return ["city_id", "city_name"]
 
 
 def read_files(files: list):
